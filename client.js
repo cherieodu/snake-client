2d269d809212525f620f5029d9ab78631de28849
Chérie Oduwole <bellapette0@gmail.com>
2021-11-17 05:10:52 +0000

    moved connect function to client.js

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '192.168.56.1', // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
